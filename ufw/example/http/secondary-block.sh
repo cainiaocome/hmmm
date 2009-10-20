08d9e681987a25cccf5b3c27b8e62570a471b86d
klzgrad <klzgrad@7d9574e0-8b31-11de-b0b7-87b715de9b2a>
2009-10-20 09:55:27 +0000

    fix an example

--- ufw/example/http/secondary-block.sh
+++ ufw/example/http/secondary-block.sh
@@ -12,4 +12,4 @@ echo 'pa;GET /falun HTTP/1.1\r\nHost: \r\n'
 #wait 2 sec, send an ack, expecting rst's
 while true; do echo '*1;_' ; done
 # usually last for 90 seconds, you need to ^C by your self
-) | ../ufw -P -w3 $1 $2
+) | ufw -aw3 $1 $2
